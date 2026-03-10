1e1e6a516970433d366d80b484faa428ac25f35b
Lekan <salamiolamilekan8991@gmail.com>
2026-03-10 15:32:29 +0100

    feat: render API data cleanly in country cards

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const API_URL = "https://restcountries.com/v3.1";
 const nameEndpoint = (countryName) => `/name/${encodeURIComponent(countryName)}?fullText=true`;
 // const otherEndpoints = "blablabla"
-const endpointFields = "&fields=name,capital,region,subregion,population,car,timezones,currencies,languages,flags,demonyms,startofweek,unmember,idd,landlocked";
+const endpointFields = "&fields=name,capital,region,subregion,population,car,timezones,currencies,languages,flags,demonyms,startOfWeek,unmember,idd,landlocked";
 
 //app config
 app.use(express.static("public"));           
@@ -19,10 +19,79 @@ app.get("/", (req, res) => {
 });
 
 // helper functions
+const errTestObject = {
+        "flags": {
+            "png": "https://flagcdn.com/w320/rw.png",
+            "svg": "https://flagcdn.com/rw.svg",
+            "alt": "The flag of Rwanda is composed of three horizontal bands of light blue, yellow and green. The light blue band is twice the height of the other two bands and bears a yellow sun with twenty-four rays on its fly side."
+        },
+        "name": {
+            "common": "Rwanda",
+            "official": "Republic of Rwanda",
+            "nativeName": {
+                "eng": {
+                    "official": "Republic of Rwanda",
+                    "common": "Rwanda"
+                },
+                "fra": {
+                    "official": "République rwandaise",
+                    "common": "Rwanda"
+                },
+                "kin": {
+                    "official": "Repubulika y'u Rwanda",
+                    "common": "Rwanda"
+                }
+            }
+        },
+        "currencies": {
+            "RWF": {
+                "name": "Rwandan franc",
+                "symbol": "Fr"
+            }
+        },
+        "languages": {
+            "eng": "English",
+            "fra": "French",
+            "kin": "Kinyarwanda"
+        },
+        "demonyms": {
+            "eng": {
+                "f": "Rwandan",
+                "m": "Rwandan"
+            },
+            "fra": {
+                "f": "Rwandaise",
+                "m": "Rwandais"
+            }
+        },
+        "idd": {
+            "root": "+2",
+            "suffixes": [
+                "50"
+            ]
+        },
+        "capital": [
+            "Kigali"
+        ],
+        "region": "Africa",
+        "subregion": "Eastern Africa",
+        "landlocked": true,
+        "population": 14104969,
+        "car": {
+            "signs": [
+                "RWA"
+            ],
+            "side": "right"
+        },
+        "timezones": [
+            "UTC+02:00"
+        ]
+    };
 
 const handleResponse = (res, responseObj) => {
-  console.log(responseObj.data);
+  // console.log(responseObj.data);
   res.render("index", { content: responseObj.data }); 
+  // res.render("index", { content: JSON.parse(errTestObject) }); 
   // res.render("index", { content: JSON.stringify(responseObj.data) });
 };
 
