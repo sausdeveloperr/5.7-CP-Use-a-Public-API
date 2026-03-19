feb9d3be69fc7da0ea5b13d5fbc8c3b97fb36338
Lekan <salamiolamilekan8991@gmail.com>
2026-03-19 20:44:12 +0100

    feat: add key identifier for each rendered country object

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const app = express();
 const port = 3000; 
 const API_URL = "https://restcountries.com/v3.1";
-const endpointFields = "fields=name,capital,region,subregion,population,car,timezones,currencies,languages,flags,demonyms,startOfWeek,unMember,idd,landlocked";
+const endpointFields = "fields=name,capital,cca3,region,subregion,population,car,timezones,currencies,languages,flags,demonyms,startOfWeek,unMember,idd,landlocked";
 
 //app config
 app.use(express.static("public"));           
